9a0065bcef0c0908b37b06a8108b825a1250a0a9
lyzlopess <112975441+lyzlopess@users.noreply.github.com>
2023-11-15 12:56:12 -0300

    update home_page

--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -26,30 +26,33 @@ class StoryStockHome extends StatelessWidget {
           IconButton(
             icon: Icon(Icons.settings),
             onPressed: () {
-                 Navigator.push(context, MaterialPageRoute(builder: (context) => (SettingsPage())));
-              // Navegar para a tela de configurações
+              Navigator.push(context, MaterialPageRoute(builder: (context) => SettingsPage()));
             },
           ),
         ],
       ),
-      body: Column(
-        children: [
-          // Lista de livros
-          Expanded(
-            child: ListView(
-              children: List.generate(9, (index) {
-                return ListTile(
-                  leading: Icon(Icons.book),
-                  title: Text("Livro ${index + 1}"),
-                  subtitle: Text("Autor do Livro ${index + 1}"),
-                  onTap: () {
-                    // Ação quando o livro for clicado
-                  },
-                );
-              }),
-            ),
-          ),
-        ],
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          crossAxisSpacing: 4.0,
+          mainAxisSpacing: 4.0,
+        ),
+        itemCount: 9,
+        itemBuilder: (BuildContext context, int index) {
+          if (index == 0) {
+            return BookCard(
+              bookTitle: "É assim que acaba",
+              author: "Colleen Hoover",
+              imagePath: "lib/images/book.png",
+            );
+          } else {
+            return BookCard(
+              bookTitle: "Livro ${index + 1}",
+              author: "Autor ${index + 1}",
+              imagePath: "lib/images/placeholder.png", // Substitua pelo caminho da sua imagem de espaço reservado
+            );
+          }
+        },
       ),
       bottomNavigationBar: BottomAppBar(
         child: Row(
@@ -57,27 +60,65 @@ class StoryStockHome extends StatelessWidget {
           children: [
             IconButton(
               icon: Icon(Icons.home),
-              onPressed: () {
-                // Navegar para a tela de livros
-              },
+              onPressed: () {},
             ),
             IconButton(
               icon: Icon(Icons.search),
               onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => (SearchPage())));
-
-                // Navegar para a tela de pesquisa
+                Navigator.push(context, MaterialPageRoute(builder: (context) => SearchPage()));
               },
             ),
             IconButton(
               icon: Icon(Icons.person),
               onPressed: () {
                 Navigator.push(context, MaterialPageRoute(builder: (context) => UserPage()));
-                // Navegar para o perfil do usuário
               },
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class BookCard extends StatelessWidget {
+  final String bookTitle;
+  final String author;
+  final String imagePath;
+
+  BookCard({
+    required this.bookTitle,
+    required this.author,
+    required this.imagePath,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4.0,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            imagePath,
+            width: 250.0, 
+            height: 350.0, 
+          ),
+          SizedBox(height: 4),
+          Text(
+            bookTitle,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold), 
+          ),
+          SizedBox(height: 4),
+          Text(author, style: TextStyle(fontSize: 18)), 
+          SizedBox(height: 4),
+          ElevatedButton(
+            onPressed: () {
+            },
+            child: Text("Detalhes", style: TextStyle(fontSize: 20)), 
+          ),
+        ],
       ),
     );
   }
