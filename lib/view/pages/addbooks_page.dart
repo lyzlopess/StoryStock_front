b98a7409b3d66b44eff3b3a69b8d9d84417ec251
GeovanaSilvaMat <114268738+GeovanaSilvaMat@users.noreply.github.com>
2023-11-29 14:22:35 -0300

    update add_books, pubspec

--- lib/view/pages/addbooks_page.dart
+++ lib/view/pages/addbooks_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:story_stock/view/pages/home_page.dart';
 import 'package:story_stock/view/pages/registerbook_page.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 void main() {
   runApp(StoryStock());
@@ -22,14 +23,25 @@ class AddPhotosPage extends StatefulWidget {
 }
 
 class _AddPhotosPageState extends State<AddPhotosPage> {
-  XFile? photo_book;
-  selectPhotoBook() async {
+  late List<String>
+      selectedPhotos; // Lista para armazenar caminhos das fotos selecionadas
+
+  @override
+  void initState() {
+    super.initState();
+    selectedPhotos =
+        List<String>.filled(5, ''); // Inicializa a lista com 5 elementos vazios
+  }
+
+  Future<void> pickAndUploadImage(int index) async {
     final ImagePicker picker = ImagePicker();
-    try {
-      XFile? file = await picker.pickImage(source: ImageSource.gallery);
-      if (file != null) setState(() => photo_book = file);
-    } on Exception catch (e) {
-      debugPrint(e.toString());
+    XFile? file = await picker.pickImage(source: ImageSource.gallery);
+
+    if (file != null) {
+      setState(() {
+        selectedPhotos[index] =
+            file.path; // Atualiza o caminho da foto na lista
+      });
     }
   }
 
@@ -39,68 +51,45 @@ class _AddPhotosPageState extends State<AddPhotosPage> {
       appBar: AppBar(
         title: Text('Adicione Fotos dos Livros'),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              padding: EdgeInsets.all(16.0),
-              child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 1,
-                  crossAxisSpacing: 16.0,
-                  mainAxisSpacing: 16.0,
-                ),
-                itemCount: 1,
-                itemBuilder: (BuildContext context, int index) {
-                  return PhotoIcon(onPressed: () {});
-                },
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            pickAndUploadImage(0);
+          },
+          child: Container(
+            width: 300.0,
+            height: 300.0,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.grey.shade500,
+                width: 1.0,
               ),
+              borderRadius: BorderRadius.circular(8.0),
             ),
+            child: selectedPhotos[0].isNotEmpty
+                ? Image.file(
+                    // Exibe a imagem selecionada se estiver disponível
+                    File(selectedPhotos[0]),
+                    fit: BoxFit.cover,
+                  )
+                : Icon(
+                    Icons.add,
+                    size: 40.0,
+                    color: Colors.grey,
+                  ),
           ),
-          SizedBox(height: 16.0),
-          Padding(
-            padding: EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            (RegisterBookPage()))); // Adicione a lógica para confirmar e processar as fotos aqui
-              },
-              child: Text('Confirmar'),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class PhotoIcon extends StatefulWidget {
-  final VoidCallback onPressed;
-  PhotoIcon({required this.onPressed});
-
-  @override
-  State<PhotoIcon> createState() => _PhotoIconState();
-}
-
-class _PhotoIconState extends State<PhotoIcon> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.grey.shade500,
-          width: 1.0,
         ),
-        borderRadius: BorderRadius.circular(8.0),
       ),
-      child: IconButton(
-        icon: Icon(Icons.add),
-        onPressed: widget.onPressed,
-
-        // Adicione a lógica para escolher uma foto aqui
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => RegisterBookPage(),
+            ),
+          ); // Adicione a lógica para confirmar e processar as fotos aqui
+        },
+        child: Icon(Icons.check),
       ),
     );
   }
